a5577e9d6c8c51b02886ba48c02fdd404b371f49
IUGAI <iugaialeksandrr@gmail.com>
2023-11-22 17:50:14 +0900

    implement featured and React router

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Featured from '../../components/Featured/Featured'
+
+function Home() {
+    return (
+        <div>
+            <Featured/>
+        </div>
+
+    )
+}
+
+export default Home
