97f5e367f067063cac750340dcd4719c65d3f6f2
IUGAI1398 <hhhs2332@gmail.com>
2023-11-22 23:21:25 +0900

    init slide

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured'
+import TrustedBy from '../../components/trustedBy/TrustedBy'
+import Slide from '../../components/Slide/Slide'
 
 function Home() {
     return (
         <div>
             <Featured/>
+            <TrustedBy/>
+            <Slide/>
         </div>
 
     )
